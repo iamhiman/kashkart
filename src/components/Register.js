c8b8acec6b50229f801bf15e8cec17e0dc6ba398
iamhiman <kashyap.himanshu27@rocketmail.com>
2021-03-18 11:13:01 +0530

    Add Form Submission

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom"
 
 export default function Register() {
+
+const [values, setValues] = useState({ name: "", email: "", password: "" });
+  
+    const handleInput = event => {
+        const { name, value } = event.target;
+        setValues({
+          ...values,
+          [name]: value
+        });
+        //console.log(event.target.name)
+        //console.log(event.target.value)      
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log("form submitted")
+        console.log(values)
+      };
 
     return (
         <section>
             <div className="form-container">
                 <h2>Login/Register</h2>
-                <form autoComplete="off" noValidate>
+                <form onSubmit={handleSubmit} autoComplete="off" noValidate>
                     <div className="form-input">
                         <label>Name</label>
-                        <input name="name" type="text" />
+                        <input name="name" type="text" onChange={handleInput} value={values.name} />
                         <small className="form-error">error</small>
                     </div>
 
                     <div className="form-input">
                         <label>Email</label>
-                        <input name="name" type="text" />
+                        <input name="email" type="text" onChange={handleInput} value={values.email} />
                         <small className="form-error"></small>
                     </div>
 
                     <div className="form-input">
                         <label>Password</label>
-                        <input name="password" type="password" />
+                        <input name="password" type="password" onChange={handleInput} value={values.password} />
                         <small className="form-error"></small>
                     </div>
 
